e94e14101fbbe3e270a5936d2e9dcd84b4f114fe
Jreyesh26 <83368766+Jreyesh26@users.noreply.github.com>
2021-04-28 23:55:18 -0500

    Agregar archivos de proyecto.

---
+++ RestAPI/RestAPI/MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using System.Net;
+using System.Net.Http;
+using Newtonsoft.Json;
+using RestAPI.Modelos;
+
+namespace RestAPI
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            var request = new HttpRequestMessage();
+            request.RequestUri = new Uri("http://ec2-18-231-108-168.sa-east-1.compute.amazonaws.com/");
+            request.Method = HttpMethod.Get;
+            request.Headers.Add("Accpet", "application/json");
+            var client = new HttpClient();
+            HttpResponseMessage response = await client.SendAsync(request);
+            if(response.StatusCode== HttpStatusCode.OK)
+            {
+                string content = await response.Content.ReadAsStringAsync();
+                var resultado = JsonConvert.DeserializeObject<List<employees>>(content);
+
+                MiLista.ItemsSource = resultado;
+            }
+        }
+    }
+}
